7ac5832358857dc66c6ceb13002512fcfe110122
hejianbo <2948426785@qq.com>
2023-06-05 13:44:25 +0800

    。。

--- StudyArr/src/StrArr.java
+++ StudyArr/src/StrArr.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class StrArr {
+import java.util.Arrays;
+import java.util.Random;
+
+public class StrArr {
+    public static void main(String[] args) {
+        String[] names = {"何", "李", "王", "陈"};
+        Random random = new Random();
+        for (int i = 0; i < names.length; i++) {
+            int name1 = random.nextInt(names.length);
+            String emp = names[i];
+            names[i] = names[name1];
+            names[name1] = emp;
+        }
+        System.out.println(Arrays.toString(names));
+    }
 }
