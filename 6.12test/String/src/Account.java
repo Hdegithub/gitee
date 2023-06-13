172aaaeb7cc9124d89ba1f318af8d90af8a8b1bf
hejianbo <2948426785@qq.com>
2023-06-13 12:29:18 +0800

    搜索名字

---
+++ 6.12test/String/src/Account.java
@@ -0,0 +1,10 @@
+public class Account {
+    private  String names;
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "names='" + names + '\'' +
+                '}';
+    }
+}
