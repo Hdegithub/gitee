01334224bec1dd8897a74963482b528466595b98
hejianbo <2948426785@qq.com>
2023-08-03 13:59:12 +0800

    mybatis练习

---
+++ D7.31/src/main/java/org/example/mapper/OrderMapper.java
@@ -0,0 +1,9 @@
+package org.example.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import org.example.pojo.Order;
+
+public interface OrderMapper {
+    int addOrder(Order order);
+    int updateOrder(Order order);
+}
