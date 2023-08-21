234b7de58d5366baeb643710fed755f3662922b9
hejianbo <2948426785@qq.com>
2023-08-21 23:59:44 +0800

    登陆拦截和字符集处理

---
+++ D8.14jsp/src/main/java/com/learnjsp/Filter/FilterLogin.java
@@ -0,0 +1,31 @@
+package com.learnjsp.Filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class FilterLogin implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("进入登录验证");
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resq = (HttpServletResponse) servletResponse;
+        String uri = req.getRequestURI();
+        //拦截所有，放行登陆login.jsp，"/login"
+        if (uri.contains("login")) {
+            filterChain.doFilter(req, resq);
+        } else {
+            HttpSession session = req.getSession();
+            Object user = session.getAttribute("user");
+            if (user == null) {//说明没登陆过，踢回login
+                resq.sendRedirect("login.jsp");
+            }else {
+                filterChain.doFilter(req,resq);
+            }
+        }
+    }
+}
---
+++ D8.14jsp/src/main/java/com/learnjsp/Filter/FilterSetChar.java
@@ -0,0 +1,18 @@
+package com.learnjsp.Filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/*"})
+public class FilterSetChar implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding("UTF-8");
+        servletResponse.setCharacterEncoding("UTF-8");
+        servletResponse.setContentType("text/html;charset=UTF-8");
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+}
+
