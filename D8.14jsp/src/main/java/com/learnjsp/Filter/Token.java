b71c8813621141997206c8d015bfbc9998716107
hejianbo <2948426785@qq.com>
2023-08-27 22:04:36 +0800

    token拦截

--- D8.14jsp/src/main/java/com/learnjsp/Filter/Token.java
+++ D8.14jsp/src/main/java/com/learnjsp/Filter/Token.java
@@ -1,36 +1,67 @@
 package com.learnjsp.Filter;
 
+import com.alibaba.fastjson.JSON;
 import com.auth0.jwt.interfaces.Claim;
+import com.learnjsp.constants.BrandConstant;
 import com.learnjsp.utils.JwtUtil;
+import com.learnjsp.utils.Result;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Map;
 
 @WebFilter(urlPatterns = {"/*"})
 public class Token implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        //针对token的过滤，拦截，放在过滤器中，统一处理，用来保护你的那些 需要登陆过的用户
-        //才能操作的接口
         HttpServletRequest req = (HttpServletRequest) servletRequest;
-        String token = req.getHeader("token");
-        //判断token的有效性，
-        if (token == null) {
-            //说明 此人没有登陆，试图直接访问接口，不允许
-            return;
-        }
-        try {
-            Map<String, Claim> stringClaimMap = JwtUtil.verifyToken(token);
-            Claim id = stringClaimMap.get("id");
-            if (id == null) {
-                //说明token格式不对，非法的访问
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        String requestURI = req.getRequestURI();
+        System.out.println("doFilter...");
+        resp.setHeader("Content-Type", "text/json;charset=utf-8");
+        // 直接返回给前端  ,返回给前端的统一的都是JSON
+        PrintWriter writer = resp.getWriter();
+        Result result = new Result();
+
+        if (requestURI.contains("edit") || requestURI.contains("delete")
+                || requestURI.contains("updateBrand") || requestURI.contains("addBrand")) {
+            //修改类的操作，需要登陆校验 ，需要是已经登陆的用户
+            //要求 针对这些接口的请求，要携带token  header中
+            String token = req.getHeader("token");
+            if (token == null || "".equals(token.trim())) {
+                //非法访问 ,拒绝访问
+                result.setCode(403);
+                result.setMsg("非法访问");
+                String s = JSON.toJSONString(result);
+                writer.write(s);
+                return;
+            } else {
+                try {
+                    Map<String, Claim> stringClaimMap = JwtUtil.verifyToken(token);
+
+                    if (stringClaimMap == null) {
+                        result.setCode(403);
+                        result.setMsg("非法访问");
+                        String s = JSON.toJSONString(result);
+                        writer.write(s);
+                        return;
+                    }
+                } catch (Exception e) {
+                    //token 解析报错  比如，过期 ，或者密文错误
+                    e.printStackTrace();
+                    result.setCode(403);
+                    result.setMsg("非法访问");
+                    String s = JSON.toJSONString(result);
+                    writer.write(s);
+                    return;
+                }
             }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            return;
         }
+        //放行请求
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 }
