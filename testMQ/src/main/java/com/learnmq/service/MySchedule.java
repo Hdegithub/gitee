5e6a6ac091127662252e0e32ae7ee9b2f880af0b
hejianbo <2948426785@qq.com>
2023-10-11 13:49:09 +0800

    时间变量需要定义在函数内

--- testMQ/src/main/java/com/learnmq/service/MySchedule.java
+++ testMQ/src/main/java/com/learnmq/service/MySchedule.java
@@ -11,18 +11,17 @@ public class MySchedule {
     /**
      * cron 借鉴自Linux 的Cron表达式
      * * 第一位，表示秒，取值0-59
-     *  * 第二位，表示分，取值0-59
-     *  * 第三位，表示小时，取值0-23
-     *  * 第四位，日期天/日，取值1-31
-     *  * 第五位，日期月份，取值1-12
-     *  * 第六位，星期，取值1-7，1表示星期天，2表示星期一
-     *  * 第七位，年份，可以留空，取值1970-2099
-     *
+     * * 第二位，表示分，取值0-59
+     * * 第三位，表示小时，取值0-23
+     * * 第四位，日期天/日，取值1-31
+     * * 第五位，日期月份，取值1-12
+     * * 第六位，星期，取值1-7，1表示星期天，2表示星期一
+     * * 第七位，年份，可以留空，取值1970-2099
      */
-    Date date=new Date();
-    SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    @Scheduled(cron = "*/5 * * * * *"  )
-    private void printTime(){
+    @Scheduled(cron = "*/5 * * * * *")
+    private void printTime() {
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         System.out.println(sdf.format(date));
     }
 }
